d87092e17e786e29cde1c2f8a889486a82b46c21
Prasanth React <78012902+prasanthreact@users.noreply.github.com>
2025-06-26 14:48:59 +0530

    remove react-router-dom import

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import AppRouter from "./package/components/appRouter";
 
-export * from "react-router-dom";
 export * from "./package/hooks/useParams";
 export * from "./package/components/appRouter";
 export default AppRouter;
