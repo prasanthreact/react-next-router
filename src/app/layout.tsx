30490e8351a208c04f846f35bd5283e1c94c2506
Prasanth React <78012902+prasanthreact@users.noreply.github.com>
2025-06-26 10:22:33 +0530

    type for sample updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
